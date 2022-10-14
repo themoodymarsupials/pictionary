dea7c66e9f52e5aefedefcb2f39b5d381df5fc7d
Rio Edwards <rioredwards@gmail.com>
2022-10-14 16:11:13 -0700

    Add new paths to supabase

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // import './auth/user.js';
 // import { fabric } from 'fabric';
 
+import { addPath } from './fetch-utils.js';
+
 const canvas = new fabric.Canvas('canvas');
 canvas.isDrawingMode = true;
 canvas.on('path:created', (options) => {
@@ -12,10 +14,21 @@ canvas.on('path:created', (options) => {
 const canvasSrc = new fabric.Canvas('canvas-src');
 const canvasDest = new fabric.Canvas('canvas-dest');
 canvasSrc.isDrawingMode = true;
-canvasSrc.on('path:created', () => {
+canvasSrc.on('path:created', async () => {
     const json = canvasSrc.toJSON();
-    const newPath = json.objects[json.objects.length - 1];
-    console.log(newPath);
+    const newPathIndex = json.objects.length - 1;
+    const newPath = json.objects[newPathIndex];
+
+    const newPathObj = {
+        path: newPath,
+        index: newPathIndex,
+    };
+    console.log(newPathObj);
+
+    const response = await addPath(newPathObj);
+
+    console.log(response);
+
     // this would be over web-socket to/from the db after updating:
     canvasDest.loadFromJSON(json);
 });
--- fetch-utils.js
+++ fetch-utils.js
@@ -32,3 +32,9 @@ export async function signOutUser() {
 export async function addPath(path) {
     return await client.from('drawings').insert(path).single();
 }
+
+/* 
+export function onPath(handlePath) {
+    client.from(`drawings`).on('INSERT', handlePath).subscribe();
+} 
+*/
