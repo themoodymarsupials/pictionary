1571cca35a714c885fa2a99ccf37a56e22aaefbf
karl wernsman <wernsmank@gmail.com>
2022-10-17 10:16:09 -0700

    Game detail page

--- render-ultils.js
+++ render-ultils.js
@@ -1,13 +1,17 @@
 export function renderGame(game) {
     const li = document.createElement('li');
 
+    const a = document.createElement('a');
+    a.href = `/game/?id=${game.id}`;
+
     const h2 = document.createElement('h2');
     h2.textContent = game.title;
 
     const img = document.createElement('img');
     img.src = game.image_url;
 
-    li.append(h2, img);
+    a.append(h2, img);
+    li.append(a);
 
     return li;
 }
