8ee4c63a59dc25106341b022c1daf424aac2b87a
Rio Edwards <rioredwards@gmail.com>
2022-10-17 14:32:06 -0700

    Cleaned up merge & Load canvasSrc paths from database! 💯

--- app.js
+++ app.js
@@ -5,31 +5,21 @@ import { addPath, getPaths, onPath } from './fetch-utils.js';
 /* Get DOM Elements */
 const canvasSrc = new fabric.Canvas('canvas-src');
 const canvasDest = new fabric.Canvas('canvas-dest');
+
 const colorSelector = document.getElementById('color-selector');
 const brushWidthSelector = document.getElementById('brush-width');
 const drawModeSelector = document.getElementById('draw-mode');
 const selectModeSelector = document.getElementById('select-mode');
 
 /* State */
-<<<<<<< HEAD
 canvasSrc.isDrawingMode = true;
 let paths = [];
 let error = null;
 
-=======
-const canvas = new fabric.Canvas('canvas');
-const paths = [];
 let canvasMode = 'draw';
 let drawingColor = '#000000'; // Defaults to black
 let brushWidth = 10;
 
-canvas.on('path:created', (options) => {
-    console.log(options);
-});
-
-console.log(document.querySelector('input[name="mode-selector"]:checked').value);
-
->>>>>>> canvas-testing
 /* Events */
 window.addEventListener('load', async () => {
     // Realtime Path Rendering from database
@@ -115,6 +105,7 @@ function displayPaths() {
 
     // console.log(canvasPathsObj);
     canvasDest.loadFromJSON(canvasPathsObj);
+    canvasSrc.loadFromJSON(canvasPathsObj);
 }
 
 function displayError() {
