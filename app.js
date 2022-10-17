b67762e3a24e732a7e39e26f63e7e3bbe674a80d
Rio Edwards <rioredwards@gmail.com>
2022-10-17 02:15:34 -0700

    Add brush color and width selectors

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ import { addPath, onPath } from './fetch-utils.js';
 /* Get DOM Elements */
 const canvasSrc = new fabric.Canvas('canvas-src');
 const canvasDest = new fabric.Canvas('canvas-dest');
+const colorSelector = document.getElementById('color-selector');
+const brushWidthSelector = document.getElementById('brush-width');
 
 /* State */
 const canvas = new fabric.Canvas('canvas');
-canvasSrc.isDrawingMode = true;
 const paths = [];
+let drawingColor = '#000000'; // Defaults to black
+let brushWidth = 10;
+
+// Fabric initializing stuff
+canvasSrc.isDrawingMode = true;
 
 canvas.on('path:created', (options) => {
     console.log(options);
 });
 
 /* Events */
-
 window.addEventListener('load', async () => {
     // Realtime Path Rendering from database
     onPath(async (payload) => {
@@ -31,6 +36,20 @@ window.addEventListener('load', async () => {
         // Insert paths into destination canvas
         canvasDest.loadFromJSON(copyPathObj);
     });
+    updateBrush();
+});
+
+// Change Drawing Color
+colorSelector.addEventListener('input', () => {
+    drawingColor = colorSelector.value;
+    console.log(`color changed to: ${drawingColor}`);
+    updateBrush();
+});
+// Change Brush Width
+brushWidthSelector.addEventListener('input', () => {
+    brushWidth = brushWidthSelector.value;
+    console.log(`brush width changed to: ${brushWidth}`);
+    updateBrush();
 });
 
 // Insert new paths into database
@@ -47,5 +66,10 @@ canvasSrc.on('path:created', async () => {
     const response = await addPath(newPathObj);
     console.log(response);
 });
+
+function updateBrush() {
+    canvasSrc.freeDrawingBrush.color = drawingColor;
+    canvasSrc.freeDrawingBrush.width = brushWidth;
+}
 
 /* Display Functions */
