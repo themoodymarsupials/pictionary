482dd0e52ce13e729785a5f80115b813f78af301
karl wernsman <wernsmank@gmail.com>
2022-10-17 17:21:29 -0700

    CSS styling updates

--- render-ultils.js
+++ render-ultils.js
@@ -1,16 +1,21 @@
 export function renderGame(game) {
     const li = document.createElement('li');
+    li.classList.add('nes-container');
+    li.classList.add('with-title');
+    li.classList.add('is-centered');
+    li.classList.add('is-rounded');
 
     const a = document.createElement('a');
     a.href = `/game/?id=${game.id}`;
 
-    const h2 = document.createElement('h2');
-    h2.textContent = game.title;
+    const p = document.createElement('p');
+    p.textContent = game.title;
+    p.classList.add('title');
 
     const img = document.createElement('img');
     img.src = game.image_url;
 
-    a.append(h2, img);
+    a.append(p, img);
     li.append(a);
 
     return li;
